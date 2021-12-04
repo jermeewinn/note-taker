95751cee1f8652a6ea3621b3e807e924dae3cb9b
Jeremy Huynh <jeahuynh18@gmail.com>
2021-12-04 11:19:40 -0800

    amended app.listen in ln.15 and added initial app.get fct. in ln.8-12

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const express = require('express');
 const { notes } = require('./Develop/db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
-// Navigate here in your browser to test the server: http://localhost:3001/api/animals
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+// Navigate here in your browser to test the server: http://localhost:3001
+app.get('/', (req, res) => {
+    fs.readFile('./Develop/db/db.json', 'utf8', function(err, data) {
+        res.send(data)
+    }); 
 });
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
+
+// fs.readFile('./Develop/db/db.json', 'utf8', function(err, data) {
+//     console.log(data)
+// });
+//
