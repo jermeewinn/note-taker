fa09e8bfd74cbb5635023b2003a014d0c52d1b63
Jeremy Huynh <jeahuynh18@gmail.com>
2021-12-04 19:32:44 -0800

    added app.post functionality along with middleware

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const { notes } = require('./Develop/db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // Navigate here in your browser to test the server: http://localhost:3001
-app.get('/', (req, res) => {
+// // This allows us to input CSS and Javascript files with index.html
+// app.use(express.static('public'));
+// // This allows us to parse incoming string/array data
+// app.use(express.urlencoded({ extended: true }));
+// // This allows us to parse incoming JSON data
+// app.use(express.json());
+
+
+// This allows the code to read ALL data coming from db.json
+app.get('/api/notes', (req, res) => {
     fs.readFile('./Develop/db/db.json', 'utf8', function(err, data) {
         res.send(data)
     }); 
